d9dc0f48c597c85bfe504e1aec68ab86e347f50d
Alberto Maiocco <apmaiocco@gmail.com>
2019-09-05 09:33:45 -0500

    worked out recursive depth traversal, got stat() working on file size. [am]

--- MaioccoProject1/prj1.c
+++ MaioccoProject1/prj1.c
@@ -10,25 +10,21 @@ struct StackNode {
   char* data;
   struct StackNode* next;
 };
-
 struct StackNode* newNode(char* data) {
   struct StackNode* stackNode = (struct StackNode*)malloc(sizeof(struct StackNode));
   stackNode->data = data;
   stackNode->next = NULL;
   return stackNode;
 }
-
 int isEmpty(struct StackNode* root) {
   return !root;
 }
-
 void push(struct StackNode** root, char* data) {
   struct StackNode* stackNode = newNode(data);
   stackNode->next = *root;
   *root = stackNode;
   printf("%s pushed to stack\n", data);
 }
-
 char* pop(struct StackNode** root) {
   if (isEmpty(*root)) {
     return "Sweet empty list bro.";
@@ -40,7 +36,6 @@ char* pop(struct StackNode** root) {
 
   return popped;
 }
-
 char* peek(struct StackNode* root) {
   if (isEmpty(root)) {
     return "Sweet empty list bro.";
@@ -48,36 +43,44 @@ char* peek(struct StackNode* root) {
   return root->data;
 }
 
+//recursive directory traversal
+void dtRecursive(char* dir, int indents) {
+  //Initializing directory entry, stats
+  struct dirent *dirEnt;
+  struct stat dirStats;
+
+  //open directory, error and exit if its not found
+  DIR *dirName = opendir(dir);
+  if (dirName == NULL) {
+    perror("dt: Error: Specified directory not found");
+    return;
+  }
+
+  //reading directories
+  while ((dirEnt = readdir(dirName)) != NULL) {
+    if (dirEnt->d_type == DT_DIR) {
+      char buffer[512];
+      if (strcmp(dirEnt->d_name, ".") == 0 || strcmp(dirEnt->d_name, "..") == 0) {
+        continue;
+      }
+      snprintf(buffer, sizeof(buffer), "%s/%s", dir, dirEnt->d_name);
+      stat(buffer, &dirStats);
+      printf("%d", dirStats.st_size);
+      printf("%*s%s\n", indents, "", dirEnt->d_name);
+      dtRecursive(buffer, indents + 2);
+    }
+    else {
+      printf("%d", dirStats.st_size);
+      printf("%*s%s\n", indents, "", dirEnt->d_name);
+    }
+  }
+  closedir(dirName);
+}
+
 int main(int argc, char* argv[]) {
-  //set up stack
-  struct StackNode* root = NULL;
-  // set up directory entry
-  struct dirent *de;
 
-  char buffer[512];
-  struct stat mystat;
+  dtRecursive(argv[1], 2);
 
-  //open directory
-  DIR *dr = opendir(argv[1]);
-
-  //sweet ass error handling
-  if (dr == NULL) {
-    printf("Could not open current directory");
-    return 0;
-  }
-
-  //push everything into the stack
-  while ((de = readdir(dr)) != NULL) {
-    sprintf(buffer, "%s/%s", argv[1], de->d_name);
-    stat(buffer, &mystat);
-    printf("%d", mystat.st_size);
-    printf(" %s\n", de->d_name);
-    push(&root, de->d_name);
-  }
-
-  printf("Top element is %s\n", peek(root));
-
-  closedir(dr);
   return 0;
 
 }
