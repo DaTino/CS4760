772790527240d22bd9301c54e37c3ad9f716df16
Alberto Maiocco <apmaiocco@gmail.com>
2019-09-17 16:29:24 -0500

    isSubsetSum added, mild file handling as well

---
+++ maiocco.2/logParse.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+//from geeksforgeeks
+bool isSubsetSum(int set[], int n, int sum) {
+    // The value of subset[i][j] will be true if there is a
+    // subset of set[0..j-1] with sum equal to i
+    bool subset[n+1][sum+1];
+    // If sum is 0, then answer is true
+    for (int i = 0; i <= n; i++)
+      subset[i][0] = true;
+    // If sum is not 0 and set is empty, then answer is false
+    for (int i = 1; i <= sum; i++)
+      subset[0][i] = false;
+     // Fill the subset table in botton up manner
+     for (int i = 1; i <= n; i++) {
+       for (int j = 1; j <= sum; j++) {
+         if(j<set[i-1]) {
+           subset[i][j] = subset[i-1][j];
+         }
+         if (j >= set[i-1]) {
+           subset[i][j] = subset[i-1][j] ||  subset[i - 1][j-set[i-1]];
+         }
+       }
+     }
+     /*   // uncomment this code to print table
+     for (int i = 0; i <= n; i++)
+     {
+       for (int j = 0; j <= sum; j++)
+          printf ("%4d", subset[i][j]);
+       printf("\n");
+     }*/
+     return subset[n][sum];
+}
+
+
+int main(int argc, char* argv[]) {
+
+  extern char *optarg;
+  extern int optind;
+  int opt, err = 0;
+  int hFlag = 0, iFlag = 0,
+      oFlag = 0, tFlag = 0;
+
+  //getopt stuff
+  while((opt = getopt(argc, argv, "hi:o:t")) != -1) {
+
+    switch(opt) {
+      case 'h':
+        hFlag=1;
+        printf("usage goes here\n");
+        return 0;
+      case 'i':
+        iFlag=1;
+        break;
+      case 'o':
+        oFlag=1;
+        break;
+      case 't':
+        tFlag=1;
+        break;
+    }
+  }
+
+  for (; optind < argc; optind++) {
+    printf("extra arguments: %s\n", argv[optind]);
+  }
+
+  FILE *fptr;
+  char *filename = "input.dat";
+  fptr = fopen(filename, "r");
+  if (fptr == NULL) {
+    perror("logParse: Error: File not found.\n");
+  }
+
+
+
+}
